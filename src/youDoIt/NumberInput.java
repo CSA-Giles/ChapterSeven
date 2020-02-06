f4bd535813ec8336fc2d9b4da7bd1b9055600321
cole-giles <54542504+cole-giles@users.noreply.github.com>
2020-02-06 13:51:45 -0600

    bruhpdated chapter 7

--- src/youDoIt/NumberInput.java
+++ src/youDoIt/NumberInput.java
@@ -1,5 +1,8 @@
 package youDoIt;
 
+
+import java.util.Scanner;
+
 public class NumberInput {
 
     public static void main(String[] args){
@@ -7,6 +10,15 @@ public class NumberInput {
         String inputString;
         int inputNumber;
         int result;
+        final int FACTOR = 10;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number >>>");
+        inputNumber = input.nextInt();
+        result = inputNumber * FACTOR;
+
+        System.out.print(inputNumber + " * " + FACTOR + " = " + result);
 
 
     }
